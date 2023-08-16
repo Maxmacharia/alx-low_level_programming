52aa03014fb36e50b4ec91049152c4ca87afe6bb
Maxmacharia <maxwellmaina5194@gmail.com>
2023-08-16 11:57:20 -0700

    program to print fibonacci sequence

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int hesabu;
+	unsigned long fibo1 = 0, fibo2 = 1, sum;
+
+	hesabu = 0;
+	while (hesabu < 50)
+	{
+		sum = fibo1 + fibo2;
+		printf("%lu", sum);
+		fibo1 = fibo2;
+		fibo2 = sum;
+		if (hesabu == 49)
+			printf("\n");
+		else
+			printf(",");
+		hesabu++;
+	}
+	return (0);
+}
