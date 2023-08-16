2429c35612b98ab20689b1bd66167ea5fc21a93a
Maxmacharia <maxwellmaina5194@gmail.com>
2023-08-16 14:51:54 -0700

    program that finds and prints the first 98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
-#include "main.h"
+
+/**
+ * numlength - function to print length
+ *
+ * @number: scrutinize input function
+ *
+ * Return: Always length
+ *
+ */
+
+int numlength(int number)
+{
+	int length = 0;
+
+	if (!number)
+		return (0);
+	while (number)
+	{
+		number = number / 10;
+		length = length + 1;
+	}
+	return (length);
+}
 
 /**
  * main - main function
