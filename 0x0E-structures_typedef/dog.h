49a051825101a7d790ad07650b04c639db23e4c5
Maxmacharia <maxwellmaina5194@gmail.com>
2023-09-11 14:27:17 -0700

    a new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,33 +2,30 @@
 #define DOG_H
 
 
-	/**
-	 * struct dog - information about the dog
-	 * @name: name of the dog
-	 * @age: Age of the dog
-	 * @owner: Owner of the dog
-	 * Description: Define a new type struct dog with the following elements
-	 */
-	struct dog
-	{
-		char *name;
-		float age;
-		char *owner;
-	};
+/**
+ * struct dog - information of the dog
+ * @name: name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ * Description: Define a new type struct dog with the following elements
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
 
+/**
+ * dog_d - typedef for struct dog
+ */
+typedef struct dog dog_d;
 
-	/**
-	 * dog_d - typedef for struct dog
-	 */
-	typedef struct dog dog_d;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_d *new_dog(char *name, float age, char *owner);
+void free_dog(dog_d *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
-
-	void init_dog(struct dog *d, char *name, float age, char *owner);
-	void print_dog(struct dog *d);
-	dog_d *new_dog(char *name, float age, char *owner);
-	void free_dog(dog_d *d);
-	char *_strcpy(char *dest, char *src);
-	int _strlen(char *s);
-
-
-	#endif
+#endif
